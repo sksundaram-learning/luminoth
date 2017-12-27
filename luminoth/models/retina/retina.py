Fix softmax in RetinaProposal.

luminoth/models/retina/retina.py
@@ -52,8 +52,6 @@ class Retina(snt.AbstractModule):
         self._background_divider = float(
             self._config.model.loss.background_weight_divider)
 
-        self._use_softmax = self._config.model.class_subnet.final.use_softmax
-
         self._offset = self._config.model.anchors.offset
 
         self._share_weights = self._config.model.share_weights
@@ -152,20 +150,21 @@ class Retina(snt.AbstractModule):
             class_scores.append(level_class_scores)
             all_anchors.append(anchors)
 
-        class_scores = tf.concat(class_scores, axis=0)
+        cls_score = tf.concat(class_scores, axis=0)
+        cls_prob = tf.nn.softmax(cls_score)
 
         bbox_preds = tf.concat(bbox_preds, axis=0)
         all_anchors = tf.concat(all_anchors, axis=0)
 
         pred_dict = {
             'pre_nms_prediction': {
-                'cls_scores': class_scores,
+                'cls_score': cls_score,
+                'cls_prob': cls_prob,
                 'bbox_preds': bbox_preds,
             }
         }
-
         proposal_dict = self._proposal(
-            class_scores, bbox_preds, all_anchors
+            cls_prob, bbox_preds, all_anchors
         )
 
         pred_dict['classification_prediction'] = {
@@ -202,7 +201,8 @@ class Retina(snt.AbstractModule):
         """
         with tf.name_scope('losses'):
             pred_dict = pred_dict['pre_nms_prediction']
-            cls_scores = pred_dict['cls_scores']
+            cls_score = pred_dict['cls_score']
+            cls_prob = pred_dict['cls_prob']
             cls_target = pred_dict['cls_target']
 
             bbox_preds = pred_dict['bbox_preds']
@@ -212,8 +212,10 @@ class Retina(snt.AbstractModule):
             filter_ignored = tf.not_equal(cls_target, -1.)
             cls_target = tf.boolean_mask(
                 cls_target, filter_ignored, name='mask_targets')
-            cls_scores = tf.boolean_mask(
-                cls_scores, filter_ignored, name='mask_scores')
+            cls_score = tf.boolean_mask(
+                cls_score, filter_ignored, name='mask_scores')
+            cls_prob = tf.boolean_mask(
+                cls_prob, filter_ignored, name='mask_probs')
 
             bbox_preds = tf.boolean_mask(
                 bbox_preds, filter_ignored, name='mask_proposals')
@@ -222,15 +224,15 @@ class Retina(snt.AbstractModule):
 
             tf.summary.scalar(
                 'nonbackground_n',
-                tf.count_nonzero(tf.argmax(cls_scores, axis=1)), ['retina']
+                tf.count_nonzero(tf.argmax(cls_score, axis=1)), ['retina']
             )
             cls_loss = focal_loss(
-                cls_scores, cls_target,
+                cls_score, cls_prob, cls_target,
                 self._num_classes, gamma=self._gamma,
-                weights=self._class_weight,
-                background_divider=self._background_divider,
-                use_softmax=self._use_softmax
+                background_divider=self._background_divider
             )
+            cls_loss = cls_loss * self._class_weight
+
             reg_loss = smooth_l1_loss(
                 bbox_preds, bbox_target
             )

luminoth/models/retina/retina_proposal.py
@@ -2,7 +2,6 @@ import sonnet as snt
 import tensorflow as tf
 
 from luminoth.utils.bbox_transform_tf import change_order, decode
-from luminoth.utils.safe_wrappers import safe_softmax
 
 
 class RetinaProposal(snt.AbstractModule):
@@ -28,10 +27,10 @@ class RetinaProposal(snt.AbstractModule):
 
         self._min_prob_threshold = config.min_prob_threshold
 
-    def _build(self, cls_score, bbox_preds, all_anchors):
+    def _build(self, cls_prob, bbox_preds, all_anchors):
         """
         Args:
-            cls_score: (num_proposals,)
+            cls_prob: (num_proposals,)
             bbox_preds: (num_proposals, 4)
             all_anchors: (num_proposals, 4)
 
@@ -43,13 +42,11 @@ class RetinaProposal(snt.AbstractModule):
         """
         # First we want get the most probable label for each proposal
         # We still have the background on idx 0 so we subtract 1 to the idxs.
-        proposal_label = tf.argmax(cls_score, axis=1, name='label_argmax') - 1
+        proposal_label = tf.argmax(cls_prob, axis=1, name='label_argmax') - 1
 
         # We are going to use only the non-background proposals.
         non_background_filter = tf.greater_equal(proposal_label, 0)
-        # TODO: optimize this. We're doing softmax on the same scores several
-        # times per step.
-        cls_prob = tf.nn.softmax(cls_score)
+
         prob_filter = tf.greater(
             tf.reduce_max(cls_prob, axis=1, name='reduce_probs'),
             self._min_prob_threshold,
@@ -68,8 +65,8 @@ class RetinaProposal(snt.AbstractModule):
         proposal_label = tf.boolean_mask(
             proposal_label, proposal_filter, name='mask_labels'
         )
-        cls_score = tf.boolean_mask(
-            cls_score, proposal_filter, name='mask_scores'
+        cls_prob = tf.boolean_mask(
+            cls_prob, proposal_filter, name='mask_probs'
         )
         anchors = tf.boolean_mask(
             all_anchors, proposal_filter, name='mask_anchors'
@@ -83,8 +80,8 @@ class RetinaProposal(snt.AbstractModule):
             ['retina']
         )
         # Get top `pre_nms_top_n` indices by sorting the proposals by score.
-        k = tf.minimum(self._pre_nms_top_n, tf.shape(cls_score)[0])
-        top_k = tf.nn.top_k(tf.reduce_max(cls_score, axis=1), k=k)
+        k = tf.minimum(self._pre_nms_top_n, tf.shape(cls_prob)[0])
+        top_k = tf.nn.top_k(tf.reduce_max(cls_prob, axis=1), k=k)
 
         top_preds = tf.gather(
             bbox_preds, top_k.indices, name='gather_preds'
@@ -96,9 +93,9 @@ class RetinaProposal(snt.AbstractModule):
             proposal_label, top_k.indices,
             name='gather_labels'
         )
-        top_scores = tf.gather(
-            cls_score, top_k.indices,
-            name='gather_scores'
+        top_prob = tf.gather(
+            cls_prob, top_k.indices,
+            name='gather_probs'
         )
 
         proposals = decode(top_anchors, top_preds)
@@ -108,33 +105,33 @@ class RetinaProposal(snt.AbstractModule):
         objects_tf = change_order(proposals)
 
         selected_boxes = []
-        selected_scores = []
+        selected_prob = []
         selected_labels = []
         # For each class we want to filter those objects and apply NMS to them.
         for class_id in range(self._num_classes):
             # Filter objects Tensors with class.
             class_filter = tf.equal(top_labels, class_id)
             class_objects_tf = tf.boolean_mask(objects_tf, class_filter)
-            this_class_score = tf.boolean_mask(top_scores, class_filter)
-            this_class_score = this_class_score[:, class_id + 1]
-            this_class_score = tf.reshape(this_class_score, [-1])
+            this_class_prob = tf.boolean_mask(top_prob, class_filter)
+            this_class_prob = this_class_prob[:, class_id + 1]
+            this_class_prob = tf.reshape(this_class_prob, [-1])
 
             # Apply class NMS.
             class_selected_idx = tf.image.non_max_suppression(
-                class_objects_tf, this_class_score,
+                class_objects_tf, this_class_prob,
                 self._class_max_detections,
                 iou_threshold=self._class_nms_threshold
             )
 
             # Using NMS resulting indices, gather values from Tensors.
             class_objects_tf = tf.gather(class_objects_tf, class_selected_idx)
-            this_class_score = tf.gather(
-                this_class_score, class_selected_idx)
+            this_class_prob = tf.gather(
+                this_class_prob, class_selected_idx)
 
             # We append values to a regular list which will later be transform
             # to a proper Tensor.
             selected_boxes.append(class_objects_tf)
-            selected_scores.append(this_class_score)
+            selected_prob.append(this_class_prob)
             # In the case of the class_id, since it is a loop on classes, we
             # already have a fixed class_id. We use `tf.tile` to create that
             # Tensor with the total number of indices returned by the NMS.
@@ -148,10 +145,10 @@ class RetinaProposal(snt.AbstractModule):
         # Return to the original convention.
         objects = change_order(objects_tf)
         proposal_label = tf.concat(
-            selected_labels, axis=0, name='concat_scores'
+            selected_labels, axis=0, name='concat_label'
         )
-        proposal_label_prob = safe_softmax(
-            tf.concat(selected_scores, axis=0, name='concat_scores')
+        proposal_label_prob = tf.concat(
+            selected_prob, axis=0, name='concat_prob'
         )
 
         # Get topK detections of all classes.
@@ -163,11 +160,9 @@ class RetinaProposal(snt.AbstractModule):
         top_k_proposal_label_prob = top_k.values
         top_k_objects = tf.gather(objects, top_k.indices)
         top_k_proposal_label = tf.gather(proposal_label, top_k.indices)
-        top_k_score = tf.gather(cls_score, top_k.indices)
 
         return {
             'objects': top_k_objects,
             'proposal_label': top_k_proposal_label,
-            'proposal_label_prob': top_k_proposal_label_prob,
-            'proposal_label_score': top_k_score
+            'proposal_label_prob': top_k_proposal_label_prob
         }

luminoth/utils/losses.py
@@ -1,12 +1,17 @@
 import tensorflow as tf
 
 
-def focal_loss(cls_scores, targets, num_classes, gamma=2.0,
-               weights=None, background_divider=1, use_softmax=False):
+def focal_loss(cls_score, cls_prob, targets, num_classes, gamma=2.0,
+               weights=1., background_divider=1):
     """Compute RetinaNet's focal loss.
 
+    We need both cls_score and cls_prob because Tensorflow's implementation
+    of cross entropy take scores and not probs, but we need the probs for the
+    focal weighting.
+
     Args:
-        cls_scores: shape (num_proposals, num_classes + 1)
+        cls_score: shape (num_proposals, num_classes + 1)
+        cls_prob: shape (num_proposals, num_classes + 1)
         targets: shape (num_proposals)
         num_classes: number of classes (not counting background)
         gamma: gamma parameter for focal loss.
@@ -14,16 +19,13 @@ def focal_loss(cls_scores, targets, num_classes, gamma=2.0,
             all weights will be 1.
     """
     with tf.name_scope('focal_loss'):
-        if weights is None:
-            weights = 1.
-
         targets_one_hot = tf.one_hot(
             tf.cast(targets, tf.int32),
             depth=num_classes + 1,
             name='one_hot_targets'
         )
         cross_entropy = tf.nn.softmax_cross_entropy_with_logits(
-            labels=targets_one_hot, logits=cls_scores,
+            labels=targets_one_hot, logits=cls_score,
             name='compute_cross_entropy'
         )
         weights = tf.where(
@@ -34,13 +36,11 @@ def focal_loss(cls_scores, targets, num_classes, gamma=2.0,
         weighted_cross_entropy = tf.multiply(
             cross_entropy, weights, name='apply_weights'
         )
-        if use_softmax:
-            cls_scores = tf.nn.softmax(cls_scores)
         # Reduce max could be switched with reduce_sum, etc. as we're only
         # getting one element per row.
         focal_weights = tf.pow(
             1. - tf.reduce_max(
-                tf.multiply(cls_scores, targets_one_hot), axis=1
+                tf.multiply(cls_prob, targets_one_hot), axis=1
             ),
             gamma, name='power_gamma'
         )